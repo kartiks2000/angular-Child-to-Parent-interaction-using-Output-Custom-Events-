33a94b60c09ca8150e10bde38909a3cc3e09b4d5
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-07-30 16:28:17 +0530

    Add files via upload

---
+++ child/child.component.ts
@@ -0,0 +1,18 @@
+import { Component,Output, EventEmitter } from '@angular/core';
+
+@Component({
+    selector: 'child-cmp',
+    templateUrl: './child.component.html'
+})
+export class childComponent{
+
+    na = "";
+
+    @Output sendingname = new EventEmitter();
+
+    changing = (event) => {
+        this.na = event.target.value;
+        this.sendingname.emit(this.na);
+    }
+
+}
